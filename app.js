d8d5e10f4222fd22b6ea76860d835d90b88edfae
SanjanaHarikantra <115627866+SanjanaHarikantra@users.noreply.github.com>
2024-12-04 13:14:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+const express = require('express');
+const articleRoutes = require('./routes/articleRoutes'); // Import article routes
+
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Use the article routes
+app.use(articleRoutes);
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+=======
+const express = require('express');
+const articleRoutes = require('./routes/articleRoutes'); // Import article routes
+
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Use the article routes
+app.use(articleRoutes);
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+>>>>>>> b4c1e402fb44016ef0e7eadc3c9e4750f32561a5
